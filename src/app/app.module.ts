789f60bd9df993b881100d4ef51e816ada335f5f
kyle beavin <kylebeavin@gmail.com>
2018-05-02 16:25:52 -0400

    routing kb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,25 @@ import { AppComponent } from './app.component';
 import { BoardsComponent } from './boards/boards.component';
 import { BoardDetailComponent } from './boards/board-detail/board-detail.component';
 import { BoardService } from './board.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardsComponent,
-    BoardDetailComponent
+    BoardDetailComponent,
+    MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [
-    BoardService
+    BoardService,
+    MessageService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/board.service.ts
+++ src/app/board.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { Boards } from './boards/boards';
 import { BOARDS } from './boards/board-data';
 import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/Observable/of';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class BoardService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getBoards(): Observable<Boards[]> {
+    this.messageService.add('BoardService: fetched boards')
     return of(BOARDS);
   }
 
